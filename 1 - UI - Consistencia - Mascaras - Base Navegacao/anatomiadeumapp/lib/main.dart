137ac2f41d5abea5c894b458094c6487883d88ec
marcioscbnu <marcioschoenfelder@gmail.com>
2025-11-05 11:34:29 -0300

    Material de Apoio

---
+++ 1 - UI - Consistencia - Mascaras - Base Navegacao/anatomiadeumapp/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Estrutura Básica Flutter',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> _opcoesDeTela = <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Consegui alterar e fazer funcionar meu primeiro app e Flutter',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    ),
+    const Center(
+      child: Text('Configurações da App', style: TextStyle(fontSize: 24)),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Meu Primeiro App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: _opcoesDeTela[0], // Display the first widget in _opcoesDeTela
+    );
+  }
+}
